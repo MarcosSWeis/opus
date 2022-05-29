f56fa0672ea7a92ae2ed2a66e66bc0d9b81e7fa9
Jair Avila <jair_p1020@hotmail.com>
2022-05-28 19:27:12 -0500

    route depart

--- server/src/routes/departs.js
+++ server/src/routes/departs.js
@@ -1,17 +1,18 @@
 const { Router } = require("express");
-
+const {Department, User} = require ("../db")
 
 const router = Router();
 
 router.get ("/onsale", async (req, res) => {
   try {
     const departOnsale = await Department.findAll({
-      include: {model:User, attributes: ['first_name', 'last_name', 'email', '']},
       where: {
         onsale: true,
-    }
-    
+      },  
+      include: {model: User, as:"user" , attributes: ["first_name", "last_name", "email", "cel"]},
     });
+
+    res.json (departOnsale)
   } catch (error) {
     res.status(500).json({
       message: "Error al obtener los departamentos",
