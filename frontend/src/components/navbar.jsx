c2ca76309f71ae2360e2f6ee7e2be0eb094d9da7
lucas <amaya.jorgelucas@gmail.com>
2022-05-29 15:49:08 -0300

    agregamos animaciones al los navLink

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -7,7 +7,7 @@ const Navbar  = () => {
         <>
                     <nav class="navbar navbar-expand-lg bg-light">
                                     <div class="container-fluid">
-                                                <a class="navbar-brand" href="#"> <img src={Logo} alt="" /></a>
+                                                <a class="navbar-brand" href="#"> <img src={Logo} alt="Logo de la empresa" /></a>
                                                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                                 <span class="navbar-toggler-icon"></span>
                                                 </button>
@@ -24,7 +24,7 @@ const Navbar  = () => {
                                                                 </li>
                                                     </ul>
                                                         <div>
-                                                                <button class="btn btn-outline-danger botonLogin   mx-5"   type="submit"><img src={Usuario} alt="" /></button>
+                                                                <button class="btn btn-outline-danger botonLogin   mx-5"   type="submit"><img src={Usuario} alt=" Imagen del login" /></button>
                                                             </div>
                                             </div>
                                     </div>
