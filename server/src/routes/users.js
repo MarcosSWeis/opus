10e279e6919a2921ef8b80481c6f1c990e9b8fcd
zerontec <zerontecnologia1@gmail.com>
2022-05-30 21:17:08 -0400

    added middleware

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { signIn } = require("../controllers/auth");
 const {
   updateUser,
   createUser,
@@ -7,13 +8,15 @@ const {
   getUsers,
 } = require("../controllers/user.controller");
 const { checkDuplicateEmailOrDni } = require("../middleware/verifyCreate");
+const { verifyToken, isAdmin, isUser } = require("../middleware/authJwt");
 const router = Router();
 
 /* Creating a router object and then using it to define the routes for the API. */
-router.get("/all", getUsers);
-router.put("/update/:id", updateUser);
+router.get("/all",[verifyToken,isAdmin], getUsers);
+router.put("/update/:id",[verifyToken ], updateUser);
 router.post("/create", [checkDuplicateEmailOrDni], createUser);
-router.get("/get/:id", getUserById);
-router.delete("/delete/:id", deleteUser);
+router.get("/get/:id",[verifyToken,isAdmin], getUserById);
+router.delete("/delete/:id",[verifyToken,isAdmin], deleteUser);
+router.post('/signin', signIn)
 
 module.exports = router;
