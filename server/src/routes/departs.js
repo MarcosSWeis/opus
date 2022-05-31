c4a8d5eb5cf210c1d2493ae08e43366ef6e40f01
Jair Avila <jair_p1020@hotmail.com>
2022-05-30 21:42:40 -0500

    filters departs

--- server/src/routes/departs.js
+++ server/src/routes/departs.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
+const { Op } = require("sequelize");
 const {Department, User, Tower} = require ("../db")
 
 const router = Router();
 
-router.get ("/onsale", async (req, res) => {
+/* router.get ("/onsale", async (req, res) => {
   try {
     const departOnsale = await Department.findAll({
       where: {
@@ -19,7 +20,7 @@ router.get ("/onsale", async (req, res) => {
       error,
     });
   }
-});
+}); */
 
 router.get ("/all",async (req, res)=>{
   try {
@@ -38,9 +39,32 @@ router.get ("/all",async (req, res)=>{
 })
 
 router.get ("/filter", async (req, res)=>{
-  
-})
+  const filters={
+    onsale:true
+  };
+  req.body.toilets?filters.toilets=req.body.toilets:null;
+  req.body.rooms?filters.rooms=req.body.rooms:null;
+  req.body.floor?filters.floor=req.body.floor:null;
+  req.body.price?filters.price={[Op.between]:req.body.price}:null;
+  req.body.size?filters.size={[Op.between]:req.body.size}:null;
+  try {
+    const departOnsale = await Department.findAll(
+      {
+      where: filters,  
+      include: {model: User, as:"user" , attributes: ["first_name", "last_name", "email", "cel"]},
+    });
 
+    res.json (departOnsale)
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al obtener los departamentos",
+      error,
+    });
+  }
+
+
+
+})
 
 
 module.exports = router;
