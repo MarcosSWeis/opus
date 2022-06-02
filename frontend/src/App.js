f1829366e726b8bdaeb71a4a12e69e004f736568
Jair Avila <jair_p1020@hotmail.com>
2022-06-02 00:04:15 -0500

    cards departs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/navbar";
 import LoginForm from "./components/loginform";
 import Nosotros from "./components/nosotros";
 import Contacto from "./components/contacto";
-import Departaments from "./components/Departaments";
+import Departaments from "./components/departaments/departaments";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
