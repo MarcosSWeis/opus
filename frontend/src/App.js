1f00041ed36a8958a7241a5e51326b70e3b62c7d
Jair Avila <jair_p1020@hotmail.com>
2022-06-02 21:44:38 -0500

    Home1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,6 @@
 import React from 'react';
 import "./App.css";
 import './styles/contacto.css' 
-import Body from './components/body'
-import Footer from './components/footer'
-import Carrusel from './components/carrusel';
 import Navbar from "./components/navbar";
 import LoginForm from "./components/loginform";
 import Nosotros from "./components/nosotros";
@@ -12,6 +9,7 @@ import Contacto from "./components/contacto";
 import Departaments from "./components/departaments/departaments";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./styles/contacto.css";
+import Home from './components/Home/Home';
 
 
 function App() {
@@ -20,24 +18,22 @@ function App() {
       <div>
         <Navbar />
         <Routes>
+
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginForm />} />
-         
           <Route path="/about" element={<Nosotros />} />
           <Route path="/depataments" element={<Departaments />} />
           <Route path="/contact" element={<Contacto />} />
 
-
-    
-      
         </Routes>
 
 
-        <Carrusel/>
-      <Body/>
+
+
    
       
     
-      <Footer/>
+
 
     
       </div>
