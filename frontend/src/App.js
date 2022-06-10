c938930ca127e1a8266ff11d188e9aea6076aedb
zerontec <zerontecnologia1@gmail.com>
2022-06-10 09:58:17 -0400

    I did cahnges en path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import "./styles/contacto.css";
 import Navbar from "./components/navbar";
-import LoginForm from "./components/loginform";
+import LoginForm from "./components/LoginForm/loginform";
 import Nosotros from "./components/nosotros";
 import Contacto from "./components/contacto";
 import Footer from "./components/footer";
@@ -10,20 +10,44 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./styles/contacto.css";
 import Home from "./components/Home/Home";
 import Departaments from "./components/Departaments";
+import Profile from "./components/Profile/Profile";
+import BoardUser from "./components/BoardUser/BoardUser";
+import BoardAdmin from "./components/BoardAdmin/BoardAdmin";
+
+
 
 function App() {
+
+/* const {isLoggedIn} = useSelector((state)=> state)
+const routing= useRoutes(routes(isLoggedIn));  //routes(isLoggedIn)  */
+
+
   return (
     <BrowserRouter>
       <div>
-        <Navbar />
+        
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/about" element={<Nosotros />} />
-          <Route path="/departments" element={<Departaments />} />
-          <Route path="/contact" element={<Contacto />} />
+          <Route path="/" element={<>  <Navbar/>  <Home />   <Footer />  </> } />
+          <Route path="/login" element={ <>  <Navbar/><LoginForm />  <Footer />  </>} />
+          <Route path="/about" element={  <>  <Navbar/>  <Nosotros />  <Footer />  </>   } />
+          <Route path="/departments" element={<>  <Navbar/>   <Departaments />  <Footer />  </>} />
+          <Route path="/contact" element={  <>  <Navbar/><Contacto />  <Footer />  </>} />
+        
+  
         </Routes>
-        <Footer />
+      
+<Routes>
+
+
+        <Route path="dashboard" element={<BoardAdmin/>}>
+        <Route
+          path="profile"
+          element={<Profile/>}
+        />
+        <Route path="user" element={<BoardUser />} />
+      </Route>
+      </Routes>
+      
       </div>
     </BrowserRouter>
   );
