f3e915e32af3c0fb140704dd2dbdb0aa493b358b
zerontec <zerontecnologia1@gmail.com>
2022-06-10 10:01:18 -0400

    I did reducer for login

--- frontend/src/redux/reducer.js
+++ frontend/src/redux/reducer.js
@@ -1,28 +1,73 @@
-import { GET_APARTAMENTS_ONSALE, GET_IMAGE_CARROUSEL , GET_IMAGE_BODY } from "./actions";
+import {
+  GET_APARTAMENTS_ONSALE,
+  GET_IMAGE_CARROUSEL,
+  GET_IMAGE_BODY,
+  SET_MESSAGE,
+  CLEAR_MESSGE,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+} from "./actions";
+
+const user = JSON.parse(localStorage.getItem("user"));
 
 export const initialState = {
+  user: user
+    ? { isLoggedIn: true, ...user }
+    : { isLoggedIn: false, user: null },
   imgCarrusel: [],
   departFilter: [],
   imgBody: [],
 };
 
 export function rootReducer(state = initialState, action) {
-  switch (action.type) {
+  switch ((action.type, action.payload)) {
     case GET_APARTAMENTS_ONSALE:
       return {
         ...state,
         departFilter: action.payload,
       };
-    case GET_IMAGE_CARROUSEL:  
+    case GET_IMAGE_CARROUSEL:
       return {
         ...state,
         imgCarrusel: action.payload,
-      }
+      };
     case GET_IMAGE_BODY:
       return {
         ...state,
         imgBody: action.payload,
-      }  
+      };
+
+    case SET_MESSAGE:
+      return {
+        message: action.payload,
+      };
+    case CLEAR_MESSGE:
+      return {
+        message: "",
+      };
+
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: action.payload.user,
+      };
+
+    case LOGIN_FAIL:
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+      };
+
+    case LOGOUT:
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+      };
+
     default:
       return state;
   }
